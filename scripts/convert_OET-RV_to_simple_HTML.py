08f1f92b15b1d5dd38d3d568ddb9bee025c4c5d8
Robert <Freely.Given.org+BOS@gmail.com>
2025-02-24 08:07:08 +1300

    Bring all files up-to-date with corrected USFM character nesting

--- scripts/convert_OET-RV_to_simple_HTML.py
+++ scripts/convert_OET-RV_to_simple_HTML.py
@@ -32,7 +32,8 @@ CHANGELOG:
     2023-09-05 Allow for /sig style
     2024-01-27 Check for unexpected USFM character formatting in /rem lines
     2025-01-19 Handle /mt3
-    2025-01-21 Better handle OT ESFM word-table
+    2025-01-21 Better handling OT ESFM word-table
+    2025-02-24 Better handling of nested USFM character markers
 """
 from gettext import gettext as _
 from typing import List, Tuple, Optional
@@ -54,10 +55,10 @@ from BibleOrgSys.Reference.BibleBooksCodes import BOOKLIST_OT39, BOOKLIST_NT27, 
 from BibleOrgSys.Reference.BibleOrganisationalSystems import BibleOrganisationalSystem
 
 
-LAST_MODIFIED_DATE = '2025-01-21' # by RJH
+LAST_MODIFIED_DATE = '2025-02-24' # by RJH
 SHORT_PROGRAM_NAME = "Convert_OET-RV_to_simple_HTML"
 PROGRAM_NAME = "Convert OET-RV ESFM to simple HTML"
-PROGRAM_VERSION = '0.78'
+PROGRAM_VERSION = '0.79'
 PROGRAM_NAME_VERSION = '{} v{}'.format( SHORT_PROGRAM_NAME, PROGRAM_VERSION )
 
 DEBUGGING_THIS_MODULE = False
@@ -1137,30 +1138,31 @@ def convert_ESFM_to_simple_HTML( BBB:str, usfm_text:str, word_table:Optional[Lis
         book_html = convert_ESFM_words( BBB, book_html, word_table )
 
     # Add character formatting
-    book_html = book_html.replace( '\\bk ', '<span class="bk">' ) \
-                         .replace( '\\bk*', '</span>' ) \
-                         .replace( '\\em ', '<em>' ) \
-                         .replace( '\\em*', '</em>' ) \
-                         .replace( '\\it ', '<i>' ) \
-                         .replace( '\\it*', '</i>' ) \
-                         .replace( '\\bd ', '<b>' ) \
-                         .replace( '\\bd*', '</b>' ) \
-                         .replace( '\\bdit ', '<b><i>' ) \
-                         .replace( '\\bdit*', '</i></b>' ) \
-                         .replace( '\\add ', '<span class="RVadded">' ).replace( '\\+add ', '<span class="RVadded">' ) \
-                         .replace( '\\add*', '</span>' ).replace( '\\+add*', '</span>' ) \
-                         .replace( '\\nd ', '<span class="nominaSacra">' ).replace( '\\+nd ', '<span class="nominaSacra">' ) \
-                         .replace( '\\nd*', '</span>' ).replace( '\\+nd*', '</span>' ) \
-                         .replace( '\\wj ', '<span class="wj">' ) \
-                         .replace( '\\wj*', '</span>' ) \
-                         .replace( '\\sc ', '<span class="sc">' ) \
-                         .replace( '\\sc*', '</span>' ) \
-                         .replace( '\\sig ', '<span class="sig">' ) \
-                         .replace( '\\sig*', '</span>' ) \
-                         .replace( '\\tl ', '<span class="tl">' ) \
+    book_html = ( book_html.replace( '\\bk ', '<span class="bk">' )
+                         .replace( '\\bk*', '</span>' )
+                         .replace( '\\em ', '<em>' ).replace( '\\+em ', '<em>' )
+                         .replace( '\\em*', '</em>' ).replace( '\\+em*', '</em>' )
+                         .replace( '\\it ', '<i>' ).replace( '\\+it ', '<i>' )
+                         .replace( '\\it*', '</i>' ).replace( '\\+it*', '</i>' )
+                         .replace( '\\bd ', '<b>' ).replace( '\\+bd ', '<b>' ) # Not actually required in OET-RV AFAWK
+                         .replace( '\\bd*', '</b>' ).replace( '\\+bd*', '</b>' )
+                         .replace( '\\bdit ', '<b><i>' ).replace( '\\+bdit ', '<b><i>' ) # Not actually required in OET-RV AFAWK
+                         .replace( '\\bdit*', '</i></b>' ).replace( '\\bdit*', '</i></b>' )
+                         .replace( '\\add ', '<span class="RVadded">' ).replace( '\\+add ', '<span class="RVadded">' )
+                         .replace( '\\add*', '</span>' ).replace( '\\+add*', '</span>' )
+                         .replace( '\\nd ', '<span class="nominaSacra">' ).replace( '\\+nd ', '<span class="nominaSacra">' )
+                         .replace( '\\nd*', '</span>' ).replace( '\\+nd*', '</span>' )
+                         .replace( '\\wj ', '<span class="wj">' ).replace( '\\+wj ', '<span class="wj">' )
+                         .replace( '\\wj*', '</span>' ).replace( '\\+wj*', '</span>' )
+                         .replace( '\\sc ', '<span class="sc">' ).replace( '\\sc ', '<span class="sc">' ) # Not actually required in OET-RV AFAWK
+                         .replace( '\\sc*', '</span>' ).replace( '\\sc*', '</span>' )
+                         .replace( '\\sig ', '<span class="sig">' )
+                         .replace( '\\sig*', '</span>' )
+                         .replace( '\\tl ', '<span class="tl">' )
                          .replace( '\\tl*', '</span>' ) \
-                         .replace( '\\no ', '<span class="no">' ) \
+                         .replace( '\\no ', '<span class="no">' )
                          .replace( '\\no*', '</span>' )
+                    )
     book_html = livenJMPs( BBB, book_html )
     book_html = livenIORs( BBB, book_html )
     assert '\\' not in book_html, f"{BBB} {book_html[book_html.index(f'{BACKSLASH}')-20:book_html.index(f'{BACKSLASH}')+22]}"
