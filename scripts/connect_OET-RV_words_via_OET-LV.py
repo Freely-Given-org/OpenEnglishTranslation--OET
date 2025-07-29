fd99a2149ab70c2be96002611311d05470c13863
Robert Hunt <Freely.Given.org@gmail.com>
2025-07-29 12:25:08 +0100

    Jul 29 drafting

--- scripts/connect_OET-RV_words_via_OET-LV.py
+++ scripts/connect_OET-RV_words_via_OET-LV.py
@@ -75,7 +75,7 @@ sys.path.insert( 0, '../../BibleTransliterations/Python/' ) # temp until submitt
 from BibleTransliterations import load_transliteration_table, transliterate_Hebrew, transliterate_Greek
 
 
-LAST_MODIFIED_DATE = '2025-06-26' # by RJH
+LAST_MODIFIED_DATE = '2025-07-25' # by RJH
 SHORT_PROGRAM_NAME = "connect_OET-RV_words_via_OET-LV"
 PROGRAM_NAME = "Connect OET-RV words to OET-LV word numbers"
 PROGRAM_VERSION = '0.78'
@@ -533,7 +533,7 @@ def connect_OET_RV( rv, lv, OET_LV_ESFM_InputFolderPath ):
     totalSimpleListedAdds = totalProperNounAdds = totalFirstPartMatchedAdds = totalManualMatchedAdds = 0
     totalSimpleListedAddsNS = totalProperNounAddsNS = totalFirstPartMatchedAddsNS = totalManualMatchedAddsNS = 0 # Nomina sacra
     for BBB,lvBookObject in lv.books.items():
-        if BBB not in ('PSA','ECC',): continue
+        if BBB not in ('PSA','EZE',): continue
         vPrint( 'Normal', DEBUGGING_THIS_MODULE, f"  Processing connect words for OET {BBB}…" )
 
         bookSimpleListedAdds = bookProperNounAdds = bookFirstPartMatchedAdds = bookManualMatchedAdds = 0
@@ -634,7 +634,7 @@ def connect_OET_RV( rv, lv, OET_LV_ESFM_InputFolderPath ):
                         logging.critical( f"Seems we have no OET-LV {BBB} {c}:{v} -- versification issue?" )
                         halt
                         continue
-                    # if BBB=='PSA' and v<3: # and c in (3,23,29) 
+                    # if BBB=='PSA' and v<3: # and c in (3,23,29)
                     #     dPrint( 'Normal', DEBUGGING_THIS_MODULE, f"\nRV entries for {BBB} {C}:{V}: ({len(rvVerseEntryList)}) {rvVerseEntryList}")
                     #     dPrint( 'Normal', DEBUGGING_THIS_MODULE, f"LV entries for {BBB} {C}:{V}: ({len(lvVerseEntryList)}) {lvVerseEntryList}")
 
